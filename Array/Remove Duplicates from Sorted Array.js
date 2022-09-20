bd6605e754922d1608a4eafbf72f0ea4205f1d39
esmoon <sorryhanlul@mail.ru>
2022-09-20 09:24:38 +0300

    small changes

--- Array/Remove Duplicates from Sorted Array.js
+++ Array/Remove Duplicates from Sorted Array.js
@@ -3,6 +3,8 @@
 // Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
+// Надо вернуть кол-во уникальных элементов
+
 const array = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4, 4, 4, 4, 5, 5];
 //  minIndex      ^
 //   current      ^
